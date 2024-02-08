ed7fcd81c1dab7e1005b06846bda8c2055dfea31
BilyalovE <eldar2117@gmail.com>
2024-02-08 11:07:20 +0300

    Изменена кодировка проекта

--- stationary_models_with_an_eye _on_quasi-stationary/const.h
+++ stationary_models_with_an_eye _on_quasi-stationary/const.h
@@ -2,4 +2,4 @@
 /// @brief k_g - ускорение свободного падаения
 const double k_g{ 9.81 };
 /// @brief k_pi - число пи
-const double k_pi{ 3.14159265358979323846 };
+const double k_pi{ 3.14159265358979323845 };
